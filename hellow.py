695e6da791b1df258f26f7caa403d85b1aaf2d9d
mahesh-shivatronics <shivatronics123@gmail.com>
2026-05-14 03:38:49 -0700

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -1,65 +1,27 @@
-from tkinter import *
+import streamlit as st
 
-# Create window
-root = Tk()
-root.title("Calculator")
-root.geometry("300x400")
+st.title("Calculator")
 
-# Entry box
-entry = Entry(root, width=20, font=("Arial", 20), borderwidth=5, justify=RIGHT)
-entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+a = st.number_input("First Number")
+b = st.number_input("Second Number")
 
-# Function to click buttons
-def button_click(number):
-    current = entry.get()
-    entry.delete(0, END)
-    entry.insert(0, str(current) + str(number))
+operation = st.selectbox(
+    "Choose Operation",
+    ["Add", "Subtract", "Multiply", "Divide"]
+)
 
-# Clear screen
-def button_clear():
-    entry.delete(0, END)
+if st.button("Calculate"):
 
-# Calculate result
-def button_equal():
-    try:
-        result = eval(entry.get())
-        entry.delete(0, END)
-        entry.insert(0, result)
-    except:
-        entry.delete(0, END)
-        entry.insert(0, "Error")
+    if operation == "Add":
+        result = a + b
 
-# Button design
-buttons = [
-    '7', '8', '9', '/',
-    '4', '5', '6', '*',
-    '1', '2', '3', '-',
-    '0', 'C', '=', '+'
-]
+    elif operation == "Subtract":
+        result = a - b
 
-row = 1
-col = 0
+    elif operation == "Multiply":
+        result = a * b
 
-for button in buttons:
+    elif operation == "Divide":
+        result = a / b if b != 0 else "Cannot divide by zero"
 
-    action = lambda x=button: (
-        button_clear() if x == 'C'
-        else button_equal() if x == '='
-        else button_click(x)
-    )
-
-    Button(root,
-           text=button,
-           width=5,
-           height=2,
-           font=("Arial", 18),
-           command=action).grid(row=row, column=col, padx=5, pady=5)
-
-    col += 1
-
-    if col > 3:
-        col = 0
-        row += 1
-
-# Run app
-root.mainloop()
+    st.write("Result:", result)
